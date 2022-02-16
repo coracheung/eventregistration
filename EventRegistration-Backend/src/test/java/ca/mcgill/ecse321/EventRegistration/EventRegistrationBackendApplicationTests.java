c4cb5382b9565216c7f456a671e3ab2990092559
cora cheung <hoi.k.cheung@mail.mcgill.ca>
2022-02-16 01:31:28 -0500

    again

--- EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/EventRegistration/EventRegistrationBackendApplicationTests.java
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/EventRegistration/EventRegistrationBackendApplicationTests.java
@@ -3,7 +3,7 @@ package ca.mcgill.ecse321.EventRegistration;
 //import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class EventRegistrationBackendApplicationTests {
 
 	@Test
